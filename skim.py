c3793595b3e244d03c06117122d72a2630e72c15
Alex Gontar <mosegontar@gmail.com>
2017-01-18 00:35:10 -0500

    Add docstrings

--- skim.py
+++ skim.py
@@ -3,8 +3,20 @@ import sys
 import os
 import re
 
+"""
+Skim.py
+
+Skim python files and return organized list of classes and functions used. 
+Indentation used in the original file is preserved:
+
+class Foo(object):
+    def __init__(self):
+    def dostuff(thing1, thing2):
+def bar(arg):
+"""
 
 def print_matches(matches, filename, length):
+    """Prints all class/def line matches to stdout"""
 
     print(filename.center(length, '-'))
     print
@@ -13,20 +25,24 @@ def print_matches(matches, filename, length):
     print
     print(filename.center(length, '-'))
 
-
 def check_regex(line):
+    """Check via regex if line is a class or function definition"""
+
     match = re.match('^\s*class.*:', line) or re.match('^\s*def.*:', line)
     try:
         return match.group()
     except AttributeError:
         return None
 
-def get_match(contents):
+def get_matches(contents):
+    """Returns list of class/def line matches for given file"""
+
     matches = [check_regex(line) for line in contents if check_regex(line)]
     return matches
 
 def read_file_contents(filename):
-    
+    """Open and read contents of given file"""
+
     try:
         with open(filename, 'r') as f:
             content = f.readlines()
@@ -36,15 +52,18 @@ def read_file_contents(filename):
     return content
 
 def process_files(filename):
+    """Read file contents, return tuple of 
+    ([class/def line matches], filename, longest line in matches list)"""
+
     contents = read_file_contents(filename)
-    matches= get_match(contents)    
+    matches= get_matches(contents)    
     return (matches, filename, len(max(matches, key=len)))
 
 def main():
-    
+    """Process every file name given in sys.argv[1:]"""
+
     results = []
     for arg in sys.argv[1:]:
-        files = process_files(arg)
         results.append(process_files(arg))
     if not results:
         return
